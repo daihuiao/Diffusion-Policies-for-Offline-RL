bf63e3a7fe774c59ed8e8cf5b20efbeeb60dcb9c
i611 <daihuiao@tju.edu.cn>
2023-06-04 22:02:52 +0800

    就现在这个版本，最后可以实现多峰分布的重建，amazing

--- agents/model.py
+++ agents/model.py
@@ -5,9 +5,66 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision.models
 
 from agents.helpers import SinusoidalPosEmb
 
+
+class ResidualBlock(torch.nn.Module):
+    def __init__(self, channels):
+        super(ResidualBlock, self).__init__()
+        self.channels = channels
+
+        self.linear1 = nn.Linear(channels, channels)
+        self.linear2 = nn.Linear(channels, channels)
+
+    def forward(self, x):
+        y = F.relu(self.linear1(x))
+        y = self.linear2(y)
+        return F.relu(x + y)
+class MLP_res(nn.Module):
+    """
+    MLP Model
+    """
+    def __init__(self,
+                 state_dim,
+                 action_dim,
+                 device,
+                 t_dim=16):
+
+        super(MLP_res, self).__init__()
+        self.device = device
+
+        self.time_mlp = nn.Sequential(
+            SinusoidalPosEmb(t_dim),
+            nn.Linear(t_dim, t_dim * 2),
+            nn.Mish(),
+            nn.Linear(t_dim * 2, t_dim),
+        )
+
+        input_dim = state_dim + action_dim + t_dim
+        self.mid_layer = nn.Sequential(nn.Linear(input_dim, 256),
+                                       nn.Mish(),
+                                       nn.Linear(256, 256),
+                                       nn.Mish(),
+                                       nn.Linear(256, 256),
+                                       nn.Mish(),
+                                       )
+        self.resblock = ResidualBlock(256)
+        self.resblock2 = ResidualBlock(256)
+        self.resblock3 = ResidualBlock(256)
+        self.final_layer = nn.Linear(256, action_dim)
+
+    def forward(self, x, time, state):
+
+        t = self.time_mlp(time)
+        x = torch.cat([x, t, state], dim=1)
+        x = self.mid_layer(x)
+        x = self.resblock(x)
+        x = self.resblock2(x)
+        x = self.resblock3(x)
+
+        return self.final_layer(x)
 
 class MLP(nn.Module):
     """
@@ -35,7 +92,8 @@ class MLP(nn.Module):
                                        nn.Linear(256, 256),
                                        nn.Mish(),
                                        nn.Linear(256, 256),
-                                       nn.Mish())
+                                       nn.Mish(),
+                                       torchvision.models.resnet18())
 
         self.final_layer = nn.Linear(256, action_dim)
 
--- diffusion_demo.py
+++ diffusion_demo.py
@@ -5,6 +5,8 @@ import torch
 import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import trange
+
 import wandb
 
 # wandb.init(project="diffusion_demo",entity="aohuidai",mode="disabled")
@@ -12,9 +14,12 @@ import wandb
 # wandb.init(project="diffusion_demo", entity="aohuidai")
 
 from torch.utils.tensorboard import SummaryWriter
+
 writer = SummaryWriter('runs/diffusion_demo')
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 lr = 0.0003
+# n_timesteps=5 #todo  一个对比实验
+n_timesteps = 50
 
 
 class Dataset(object):
@@ -26,48 +31,56 @@ class Dataset(object):
         ind = torch.randint(0, self.data.shape[0], size=(batch_size,))
         return torch.clamp(torch.tensor(self.data[ind], dtype=torch.float32), min=-1, max=1).to(self.device)
 
+    def append(self, date):
+        self.data = np.vstack((self.data, date))
+
 
 # 定义均值和协方差矩阵
-mean = [0.5, 0]
-cov = [[0.01, 0], [0, 0.01]]
+mean0 = [0.5, 0]
+cov0 = [[0.01, 0], [0, 0.01]]
+mean1 = [-0.5, 0]
+cov1 = [[0.01, 0], [0, 0.01]]
 
 # 生成二维高斯分布
-x, y = np.random.multivariate_normal(mean, cov, 50000).T
-dataset0 = Dataset(np.random.multivariate_normal(mean, cov, 50000), device=device)
-# 定义均值和协方差矩阵
-mean = [-0.5, 0]
-cov = [[0.01, 0], [0, 0.01]]
-dataset1 = Dataset(np.random.multivariate_normal(mean, cov, 50000), device=device)
-# 生成二维高斯分布
-x2, y2 = np.random.multivariate_normal(mean, cov, 50000).T
+dataset0 = Dataset(np.random.multivariate_normal(mean0, cov0, 50000), device=device)
+dataset1 = Dataset(np.random.multivariate_normal(mean1, cov1, 50000), device=device)
 
+# dataset0.append(np.random.multivariate_normal(mean1, cov1, 50000))
+# dataset1.append(np.random.multivariate_normal(mean0, cov0, 50000))
+
+# 生成二维高斯分布
+x0, y0 = np.random.multivariate_normal(mean0, cov0, 50000).T
+x1, y1 = np.random.multivariate_normal(mean1, cov1, 50000).T
 # 绘制二维高斯分布的散点图
-plt.scatter(x, y)
-plt.scatter(x2, y2)
+plt.scatter(x0, y0)
+plt.scatter(x1, y1)
+plt.axis('equal')
 plt.show()
 
 from agents.diffusion import Diffusion
-from agents.model import MLP
+from agents.model import MLP,MLP_res
 
-model0 = MLP(state_dim=2, action_dim=2, device=device)
+# model0 = MLP(state_dim=2, action_dim=2, device=device)
+model0 = MLP_res(state_dim=2, action_dim=2, device=device)
 actor0 = Diffusion(state_dim=1, action_dim=2, model=model0, max_action=1.0,
-                   beta_schedule='vp', n_timesteps=5,loss_type="l1" ).to(device)
+                   beta_schedule='vp', n_timesteps=n_timesteps, loss_type="l2").to(device)
 actor0_optimizer = torch.optim.Adam(actor0.parameters(), lr=lr)
 
-model1 = MLP(state_dim=2, action_dim=2, device=device)
+# model1 = MLP(state_dim=2, action_dim=2, device=device)
+model1 = MLP_res(state_dim=2, action_dim=2, device=device)
 actor1 = Diffusion(state_dim=1, action_dim=2, model=model1, max_action=1.0,
-                   beta_schedule='vp', n_timesteps=5, ).to(device)
+                   beta_schedule='vp', n_timesteps=n_timesteps, loss_type="l2").to(device)
 actor1_optimizer = torch.optim.Adam(actor1.parameters(), lr=lr)
 
 
-def train(index, actor, actor_optimizer, dataset, epoch,iterations=1000):
+def train(index, actor, actor_optimizer, dataset, epoch, iterations=1000):
     for _ in range(iterations):
-        # if random.random() < 0.9:
-        #     client_id = torch.tensor(np.ones((128, 1), dtype=np.float32) * index).to(device)
-        # else:
-        #     client_id = torch.tensor(np.ones((128, 1), dtype=np.float32) * -1).to(device)
-
-        client_id = torch.tensor(np.ones((128, 1), dtype=np.float32) * index).to(device)
+        if random.random() < 0.9:  # 0.9的概率用真实label
+            client_id = torch.tensor(np.ones((128, 1), dtype=np.float32) * index).to(device)
+        else:
+            client_id = torch.tensor(np.ones((128, 1), dtype=np.float32) * -1).to(device)
+        # 全都使用同一个label
+        # client_id = torch.tensor(np.ones((128, 1), dtype=np.float32) * -1).to(device)
         states = torch.tensor(np.random.uniform(-1, 1, size=(128, 1), ), dtype=torch.float32).to(device)
         states = torch.cat([client_id, states], dim=1)
         actions = dataset.sample(128)
@@ -77,18 +90,59 @@ def train(index, actor, actor_optimizer, dataset, epoch,iterations=1000):
         # if grad_norm > 0:
         #     actor_grad_norms = nn.utils.clip_grad_norm_(self.actor.parameters(), max_norm=self.grad_norm, norm_type=2)
         # wandb.log({f"loss{str(index)}/actor_loss": actor_loss})
-        writer.add_scalar(f"loss{str(index)}/actor_loss", actor_loss, epoch*iterations+_)  # tensorboard
+        writer.add_scalar(f"loss{str(index)}/actor_loss", actor_loss, epoch * iterations + _)  # tensorboard
         actor_optimizer.step()
     return
+
 
 global_parameters_actor = {}
 for key, parameter in actor0.model.state_dict().items():
     global_parameters_actor[key] = parameter.clone()
 actor1.model.load_state_dict(global_parameters_actor)
 
-for i in range(100):
-    train(0, actor0, actor0_optimizer, dataset0,epoch=i,iterations=100)
-    train(1, actor1, actor1_optimizer, dataset1,epoch=i,iterations=100)
+def plot_all():
+    index = 0
+    client_id = torch.tensor(np.ones((128, 1), dtype=np.float32) * index).to(device)
+    states = torch.tensor(np.random.uniform(-1, 1, size=(128, 1), ), dtype=torch.float32).to(device)
+    states = torch.cat([client_id, states], dim=1)
+    actions = actor0.sample(states)
+    x, y = actions[:, 0].cpu().detach().numpy(), actions[:, 1].cpu().detach().numpy()
+    plt.scatter(x, y)
+
+    index = 1
+    client_id = torch.tensor(np.ones((128, 1), dtype=np.float32) * index).to(device)
+    states = torch.tensor(np.random.uniform(-1, 1, size=(128, 1), ), dtype=torch.float32).to(device)
+    states = torch.cat([client_id, states], dim=1)
+    actions = actor1.sample(states)
+    x, y = actions[:, 0].cpu().detach().numpy(), actions[:, 1].cpu().detach().numpy()
+    plt.scatter(x, y)
+
+    index = -1
+    client_id = torch.tensor(np.ones((128, 1), dtype=np.float32) * index).to(device)
+    states = torch.tensor(np.random.uniform(-1, 1, size=(128, 1), ), dtype=torch.float32).to(device)
+    states = torch.cat([client_id, states], dim=1)
+    actions = actor0.sample(states)
+    x, y = actions[:, 0].cpu().detach().numpy(), actions[:, 1].cpu().detach().numpy()
+    plt.scatter(x, y)
+    plt.axis('equal')
+    plt.show()
+    haha = True
+def plot(actor0, i, index=0):
+    client_id = torch.tensor(np.ones((128, 1), dtype=np.float32) * index).to(device)
+    states = torch.tensor(np.random.uniform(-1, 1, size=(128, 1), ), dtype=torch.float32).to(device)
+    states = torch.cat([client_id, states], dim=1)
+    actions = actor0.sample(states)
+    x, y = actions[:, 0].cpu().detach().numpy(), actions[:, 1].cpu().detach().numpy()
+    plt.scatter(x, y)
+    plt.title(f"agent:{index},epoch:{i},n_timesteps:{n_timesteps},iterations:{iterations}")
+    plt.axis('equal')
+    plt.show()
+
+
+iterations = 100
+for i in trange(1000):
+    train(0, actor0, actor0_optimizer, dataset0, epoch=i, iterations=100)
+    train(1, actor1, actor1_optimizer, dataset1, epoch=i, iterations=100)
     for key, parameter in actor0.model.state_dict().items():
         global_parameters_actor[key] = parameter.clone()
     for key, parameter in actor1.model.state_dict().items():
@@ -97,30 +151,10 @@ for i in range(100):
         global_parameters_actor[key] = parameter / 2.0
     actor0.model.load_state_dict(global_parameters_actor)
     actor1.model.load_state_dict(global_parameters_actor)
-for i in range(500):
-    index=0
-    client_id = torch.tensor(np.ones((128, 1), dtype=np.float32) * index).to(device)
-    states = torch.tensor(np.random.uniform(-1, 1, size=(128, 1), ), dtype=torch.float32).to(device)
-    states = torch.cat([client_id, states], dim=1)
-    actions = actor0.sample(states)
-    x,y=actions[:,0].cpu().detach().numpy(),actions[:,1].cpu().detach().numpy()
-    plt.scatter(x, y)
-    index=1
-    client_id = torch.tensor(np.ones((128, 1), dtype=np.float32) * index).to(device)
-    states = torch.tensor(np.random.uniform(-1, 1, size=(128, 1), ), dtype=torch.float32).to(device)
-    states = torch.cat([client_id, states], dim=1)
-    actions = actor1.sample(states)
-    x,y=actions[:,0].cpu().detach().numpy(),actions[:,1].cpu().detach().numpy()
-    plt.scatter(x, y)
-
-    index=-1
-    client_id = torch.tensor(np.ones((128, 1), dtype=np.float32) * index).to(device)
-    states = torch.tensor(np.random.uniform(-1, 1, size=(128, 1), ), dtype=torch.float32).to(device)
-    states = torch.cat([client_id, states], dim=1)
-    actions = actor1.sample(states)
-    x,y=actions[:,0].cpu().detach().numpy(),actions[:,1].cpu().detach().numpy()
-    plt.scatter(x, y)
-
-    plt.show()
-    haha=True
+    if (i + 1) % 100 == 0:
+        # plot(actor0, i, index=-1)
+        #
+        # plot(actor0, i, index=0)
+        # plot(actor1, i, index=1)
+        plot_all()
 
